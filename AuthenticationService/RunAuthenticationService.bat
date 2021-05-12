b8596eb4ea98cfcee0fc34d668a7003e10ad16c8
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-12 21:40:32 +0300

    Delete old C# servers and add new Django servers for 1 and 2 labs

--- AuthenticationService/RunAuthenticationService.bat
+++
@@ -1,4 +0,0 @@
-start "AuthenticationService" cmd /K docker-compose ^
-				-f .\docker-compose.override.yml ^
-				-f .\docker-compose.yml ^
-				-p AuthenticationService up -d
--- LeaderboardService/RunLeaderboardService.bat
+++
@@ -1,4 +0,0 @@
-start "LeaderboardService" cmd /K docker-compose ^
-				-f .\docker-compose.override.yml ^
-				-f .\docker-compose.yml ^
-				-p LeaderboardService up -d
