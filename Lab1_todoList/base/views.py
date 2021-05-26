ba89f2568e97168d951ad4cc577591edd403c746
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-27 00:57:49 +0300

    Add show items list

--- Lab1_todoList/base/views.py
+++ Lab1_todoList/base/views.py
@@ -1,7 +1,10 @@
+from base.models import Task
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 # Create your views here.
 
-def taskList(request):
-    return HttpResponse('TODO list')
+class TaskList(ListView):
+    model = Task
