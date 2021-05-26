d045447fd06795443918092c6d9682af33d4b62a
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-27 02:45:00 +0300

    fix create task

--- Lab1_todoList/base/views.py
+++ Lab1_todoList/base/views.py
@@ -68,7 +68,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('tasks')
 
 
-    def form_invalid(self, form):
+    def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
