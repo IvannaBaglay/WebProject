8d8df5440ccf1c4a413fa4e0ea5c7ac7039a1600
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-28 00:16:25 +0300

    Added Sing up form

--- Vuejs/todoapp/src/App.vue
+++ Vuejs/todoapp/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <v-app>
-    <Login/>
+    <Singup/>
   </v-app>
 </template>
 
 <script>
 import Vuetify from 'vuetify'
-import Login from "./components/Auth/Login"
+//import Login from "./components/Auth/Login"
+import Singup from "./components/Auth/Singup"
 
 export default {
   name: 'app',
   vuetify: new Vuetify(),
-  components: {Login
+  components: {
+    Singup
   }
 }
 </script>
---
+++ Vuejs/todoapp/src/components/Auth/Singup.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md8>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="blue">
+            <v-toolbar-title>Signup form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            
+          </v-card-text>
+          <v-divider light></v-divider>
+          <v-card-actions>
+            <v-btn to="/login" round color="black" dark>Login</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn round color="success" @click.prevent="register()">
+              Register
+              <v-icon>keyboard_arrow_up</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+};
+</script>
