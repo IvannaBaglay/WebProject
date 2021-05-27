3319e8781f8061c3a47e8fedfcb84e753300eda9
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-28 00:11:11 +0300

    Create new project VueTodo list and added Login page

---
+++ Vuejs/todoapp/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <Login/>
+  </v-app>
+</template>
+
+<script>
+import Vuetify from 'vuetify'
+import Login from "./components/Auth/Login"
+
+export default {
+  name: 'app',
+  vuetify: new Vuetify(),
+  components: {Login
+  }
+}
+</script>
