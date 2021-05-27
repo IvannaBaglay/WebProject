72c76d22adea7aae55153a2f62293c208be9c165
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-27 05:19:57 +0300

    changes consumer added new function: connect, disconect, receive, chat_message and check all

--- Lab1_todoList/Lab1_todoList/settings.py
+++ Lab1_todoList/Lab1_todoList/settings.py
@@ -72,6 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Lab1_todoList.wsgi.application'
 ASGI_APPLICATION = "Lab1_todoList.asgi.application"
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
