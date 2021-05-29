e80c08c8eaf47c82689e9669d28703cfb49b4186
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-29 14:46:31 +0300

    Integrate cypress with example and added own test

---
+++ Vuejs2/tests/e2e/untitled.spec.js
@@ -0,0 +1,16 @@
+// untitled.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Test Links', ()=>
+{
+    beforeEach(()=>{
+        cy.visit('http://localhost:8081/')
+    })
+    it('check links', ()=> {
+        cy.get('[data-cy=links]').children().should('have.length', 2);
+    })
+})
