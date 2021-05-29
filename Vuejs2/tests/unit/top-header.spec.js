371a100cad6ac6e004c963a9a553c4d65975e98d
IvannaB <ivankamorkai.baglay@gmail.com>
2021-05-29 12:37:49 +0300

    added file with tests

---
+++ Vuejs2/tests/unit/top-header.spec.js
@@ -0,0 +1,52 @@
+import TopHeader from '@/components/Top-Header.vue'
+import {shallowMount} from '@vue/test-utils'
+import flushPromises from 'flush-promises'
+
+
+const $router = {
+    replace: jest.fn()
+
+
+}
+
+jest.mock("firebase/app", ()=> ({
+    auth(){
+        return {
+            onAuthStateChanged(fnc){
+                return fnc(true);
+            },
+            signOut: ()=> Promise.resolve()
+        }
+    }
+}))
+
+describe ("topHeader.vue", ()=> {
+    let wrapper ;
+    beforeEach(()=> {
+        wrapper = shallowMount(TopHeader, {
+            mocks:{
+                $router 
+
+            }
+            //methods: {setupFirebase: ()=> {}}
+        })
+
+    })
+
+    it("renders", ()=> {
+        expect(wrapper.exists()).toBe(true);
+
+    })
+    it("does h1 exist", ()=> {
+        expect(wrapper.find("h1").text()).toBe("Logged in");
+    })
+    it("user is logged in after setting firebase mock", ()=>{
+        expect(wrapper.vm.$data.loggedIn).toBe(true);
+    })
+    it("on signout route to correct place", async ()=> {
+        wrapper.find("button").trigger("click");
+        await flushPromises();
+        expect($router.replace).lastCalledWith ({name: "login"})
+    })
+
+})
